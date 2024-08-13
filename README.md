7a38cc90ed89f5c547103a05eb2e117c5f5b3aba
Zachary Blickensderfer <zach.blickensderfer@gmail.com>
2024-08-13 12:24:56 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# BitmapCompressor
+#### A puzzle created by Zach Blick for Adventures in Algorithms at Menlo School in Atherton, CA
+
+## Your goal
+Complete the functions, `compress()` and `expand()`.
+
+`compress()` reads in binary data from a bitmap passed through `StdIn`. It compresses it, then outputs binary data to `StdOut`.
+
+`expand()` reads in binary data from `StdIn`, expands it, and outputs binary data for a bitmap to `StdOut`.
+
+The function headers are in the `BitmapCompressor` java file. Write your code there.
+
+You can compile and run your code at the command-line. Tester files have been provided for you (in the src folder).
---
+++ src/BitmapCompressor.java
@@ -0,0 +1,63 @@
+/******************************************************************************
+ *  Compilation:  javac BitmapCompressor.java
+ *  Execution:    java BitmapCompressor - < input.bin   (compress)
+ *  Execution:    java BitmapCompressor + < input.bin   (expand)
+ *  Dependencies: BinaryIn.java BinaryOut.java
+ *  Data files:   q32x48.bin
+ *                q64x96.bin
+ *                mystery.bin
+ *
+ *  Compress or expand binary input from standard input.
+ *
+ *  % java DumpBinary 0 < mystery.bin
+ *  8000 bits
+ *
+ *  % java BitmapCompressor - < mystery.bin | java DumpBinary 0
+ *  1240 bits
+ ******************************************************************************/
+
+/**
+ *  The {@code BitmapCompressor} class provides static methods for compressing
+ *  and expanding a binary bitmap input.
+ *
+ *  @author Robert Sedgewick
+ *  @author Kevin Wayne
+ *  @author Zach Blick
+ *  @author YOUR NAME HERE
+ */
+public class BitmapCompressor {
+
+    /**
+     * Reads a sequence of bits from standard input, compresses them,
+     * and writes the results to standard output.
+     */
+    public static void compress() {
+
+        // TODO: complete compress()
+
+        BinaryStdOut.close();
+    }
+
+    /**
+     * Reads a sequence of bits from standard input, decodes it,
+     * and writes the results to standard output.
+     */
+    public static void expand() {
+
+        // TODO: complete expand()
+
+        BinaryStdOut.close();
+    }
+
+    /**
+     * When executed at the command-line, run {@code compress()} if the command-line
+     * argument is "-" and {@code expand()} if it is "+".
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+        if      (args[0].equals("-")) compress();
+        else if (args[0].equals("+")) expand();
+        else throw new IllegalArgumentException("Illegal command line argument");
+    }
+}
